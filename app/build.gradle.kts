49ef8190975e129218a26c46f8b100b5ad5aafaf
Vitkkk <asstecbp@gmail.com>
2026-07-28 14:49:23 -0300

    Release effect settings translation alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vitkkk.flptoflm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.4.0-alpha"
+        versionCode = 13
+        versionName = "0.5.0-alpha"
     }
 
     buildFeatures {
