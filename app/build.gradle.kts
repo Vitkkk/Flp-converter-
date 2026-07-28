4e84e57e114aaf10c47cfc4ec2aa26ff216dc5b6
Vitkkk <asstecbp@gmail.com>
2026-07-28 00:22:38 -0300

    Bump real FLM writer alpha version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vitkkk.flptoflm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.0-alpha"
+        versionCode = 4
+        versionName = "0.3.0-alpha"
     }
 
     buildTypes {
