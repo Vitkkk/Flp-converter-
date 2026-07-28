483a2c99a2176ce0783c294d7fc3793be41ad44b
Vitkkk <asstecbp@gmail.com>
2026-07-28 00:32:25 -0300

    Bump alpha after timing fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vitkkk.flptoflm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.3.0-alpha"
+        versionCode = 5
+        versionName = "0.3.1-alpha"
     }
 
     buildTypes {
