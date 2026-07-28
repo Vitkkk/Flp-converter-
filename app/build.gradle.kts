8f94d34a2d86c526c60300df1fe0b36767afad25
Vitkkk <asstecbp@gmail.com>
2026-07-28 09:30:18 -0300

    Bump version to 0.3.5 alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vitkkk.flptoflm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.3.4-alpha"
+        versionCode = 9
+        versionName = "0.3.5-alpha"
     }
 
     buildFeatures {
