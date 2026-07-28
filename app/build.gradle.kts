94119511f9ad252efab52e63073d550dd1ba5caa
Vitkkk <asstecbp@gmail.com>
2026-07-28 14:03:52 -0300

    Release first effect-aware alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vitkkk.flptoflm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.3.7-alpha"
+        versionCode = 12
+        versionName = "0.4.0-alpha"
     }
 
     buildFeatures {
