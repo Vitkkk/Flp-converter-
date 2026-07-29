2062e0388627db5ccb8eb1c91c3c16d7685844a8
Vitkkk <asstecbp@gmail.com>
2026-07-29 14:31:26 -0300

    Release BPM Change split alpha 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vitkkk.flptoflm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "0.5.8-alpha"
+        versionCode = 22
+        versionName = "0.6.0-alpha"
     }
 
     buildFeatures {
