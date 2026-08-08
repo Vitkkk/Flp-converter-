fa5cbb89544b38482b83d66e62d7b00947aba9f8
Vitkkk <asstecbp@gmail.com>
2026-08-08 18:10:16 -0300

    Release tempo detection fix alpha 0.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vitkkk.flptoflm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "0.6.0-alpha"
+        versionCode = 23
+        versionName = "0.6.1-alpha"
     }
 
     buildFeatures {
