42a3b1fc99ef9993fd0d74606643b7846684e5f7
Vitkkk <asstecbp@gmail.com>
2026-08-12 20:22:39 -0300

    Release optional ZIP audio UI alpha 0.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.vitkkk.flptoflm"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "0.7.0-alpha"
+        versionCode = 25
+        versionName = "0.7.1-alpha"
     }
 
     buildFeatures {
